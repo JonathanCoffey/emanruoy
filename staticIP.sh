0ec2ad81a8be3f3932c8de2a622dc22e152883e4
JonathanCoffey <JonathanCoffey1087@gmail.com>
2017-05-30 20:27:09 -0500

    updating staticIP.sh script. Improved accuracy of ethernet adapter name.

--- staticIP.sh
+++ staticIP.sh
@@ -4,8 +4,15 @@
 #!/bin/bash
 
 # this will simplify the ethernet adapter name.
-eth0=$(ifconfig | grep -i "enp" | awk '{print $1}')
+eth0=$(ifconfig | grep -i "ethernet" | awk '{print $1}')
 
+# output the new value of variable $eth0
 echo $eth0
 
+echo "iface $eth0 inet static" >> /etc/network/interfaces
+echo "address 10.1.10.12" >> /etc/network/interfaces
+echo "netmask 255.255.255.0" >> /etc/network/interfaces
+echo "gateway 10.1.10.1" >> /etc/network/interfaces
+echo "dns-nameservers 10.1.10.1" >> /etc/network/interfaces
 
+echo "Completed network static settings for 10.1.10.12"
