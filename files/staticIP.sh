0d7cecffd8417b54f591481c1e8f62909b1d23ce
JonathanCoffey <JonathanCoffey1087@gmail.com>
2017-05-31 07:45:05 -0500

    updating static ip script. Need more details from main LServer before continuing.

--- files/staticIP.sh
+++ files/staticIP.sh
@@ -9,9 +9,7 @@
 # Could see bug if wifi is only reported as it also contains string "Ethernet", need a way to exclude it.
 # Since this is for server setup and not run on clinents, ethernet should be reported first.
 eth0=$(ifconfig | grep -i "ethernet" | awk 'NR==1{print $1}')
-
-# output the new value of variable $eth0
-echo $eth0
+echo "Detected Ethernet adapter: $eth0"
 
 # Network manager does nor nmcli uses this config. Network manager would need to be removed for this to work.
 # Keeping for later reference or in case we need to find another way. 
@@ -22,3 +20,11 @@ echo $eth0
  #echo "gateway 10.1.10.1" >> /etc/network/interfaces
  #echo "dns-nameservers 10.1.10.1" >> /etc/network/interfaces
 
+# adding ethernet connection profile with manual IP configuration. nmcli
+nmcli con add con-name 10Twelve ifname $eth0 type ethernet autoconnect no
+nmcli con mod 10Twelve ipv4.addresses 10.1.10.12/24
+nmcli con mod 10Twelve ipv4.gateway 10.1.10.1
+nmcli con mod 10Twelve ipv4.dns "8.8.8.8 8.8.4.4"
+nmcli con mod 10Twelve +ipv4.dns 10.1.10.1
+ #nmcli con mod 10Twelve ipv6.dns ""
+nmcli con show 10Twelve | less
