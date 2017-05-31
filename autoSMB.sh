04abfa1671e201b9c3a845cc8881cc13771a935d
JonathanCoffey <JonathanCoffey1087@gmail.com>
2017-05-30 22:48:33 -0500

    removing stuff

--- autoSMB.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# attempt to automatically mount SMB share to client machine w/ blank usr/pass.
-mkdir ~/tk
-sudo mount -t cifs -ouser="Guest",password="",rw //10.1.10.12/smb ~/tk/
-echo "End of autoSMB script"
-sleep 2
--- bl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# comment
-upower -i /org/freedesktop/UPower/devices/battery_BAT0
-echo "*************************************************"
-upower -i /org/freedesktop/UPower/devices/battery_BAT1
-echo "*************************************************"
