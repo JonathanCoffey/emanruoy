71e4d801c189ed3285a39092a1b17f14c1c05941
JonathanCoffey <JonathanCoffey1087@gmail.com>
2017-07-16 13:18:42 -0500

    Still working on tmux

--- menu.sh
+++ menu.sh
@@ -7,16 +7,30 @@ clear
 HOSTNAME=$(hostname)
 ID=$(id -u)
 IPADDRESS=$(hostname -I | awk '{print $1}')
+jonSTATUS=$(tmux ls | grep "JonThan" | grep -i "attached" >/dev/null; echo $?)
+mainSTATUS=$(tmux ls | grep "Main" | grep -i "attached" >/dev/null; echo $?)
+
+
+
+
+# check if sudo. I left this here so that you can soure the script variables and stop the script here.;
 
 [ "$ID" -ne 0 ] && echo "This should be run as root." && sleep 2 && exit
 
+# test if IP address is zero value.
+[ -z "${IPADDRESS}" ] && echo "IP address is a zero value, make sure you're on network and have a valid IP." && sleep 2 && exit
+
+# See if any tmux sessions already running, if so, attach to it instead of starting new session.
+[ "$mainSTATUS" = 0 ] && tmux a -t "Main:menu" || tmux new-session -s "Main" $0 \; rename window "menu" \; set mouse on $
+[ "$jonSTATUS" = 0 ] && tmux a -t "JonThan:menu" || tmux new-session -s "JonThan" $0 \; rename window "menu" \; set mouse on $
+
 # check if in tmux menu
-if [ ! "$TMUX" ]; then
-    if [ "$1" == '--jon' ]; then
-	[ ! "$TMUX" ] && tmux a -t "JonThan:menu" 2>/dev/null || tmux new-session -s "JonThan" $0 \; rename-window "menu" \; set mouse on \; 2>/dev/null
-    else
-        [ ! "$TMUX" ] && tmux a -t "Main:menu" || tmux new-session -s  "Main" $0 \; rename-window "menu" \; set mouse on \; 2>/dev/null
-    fi
-fi
+#if [ ! "$TMUX" ]; then
+#    if [ "$1" == '--jon' ]; then
+#	[ ! "$TMUX" ] && tmux a -t "JonThan:menu" 2>/dev/null || tmux new-session -s "JonThan" $0 \; rename-window "menu" \; set mouse on \; 2>/dev/null
+#    else
+#        [ ! "$TMUX" ] && tmux a -t "Main:menu" || tmux new-session -s  "Main" $0 \; rename-window "menu" \; set mouse on \; 2>/dev/null
+#    fi
+#fi
 
 
