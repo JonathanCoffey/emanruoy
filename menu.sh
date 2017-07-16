8d51882204caad10c2e21b087e6dc0e066a7c7fa
JonathanCoffey <JonathanCoffey1087@gmail.com>
2017-07-15 22:03:01 -0500

    Making tmux framework.

--- menu.sh
+++ menu.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 clear
 
-
-
 #tmux new-session -s Menu 
 
 # Declare Variables
@@ -10,7 +8,7 @@ HOSTNAME=$(hostname)
 ID=$(id -u)
 IPADDRESS=$(hostname -I | awk '{print $1}')
 
-[ "$ID" -eq 0 ] && echo "This shouldn't be ran as root." && sleep 2 && exit
+[ "$ID" -ne 0 ] && echo "This should be run as root." && sleep 2 && exit
 
 # check if in tmux menu
 if [ ! "$TMUX" ]; then
@@ -21,46 +19,4 @@ if [ ! "$TMUX" ]; then
     fi
 fi
 
-#echo $HOSTNAME
-#echo $ID
-#echo $IPADDRESS
 
-
-
-
-PS3='Please select an option: '
-echo
-echo "Main Menu"
-echo
-echo
-options=("static IP" "software Update" "autoSSH/SMB" "Quit")
-select opt in "${options[@]}"
-do 
-   case $opt in
-        "static IP")
-            ./files/staticIP.sh
-            ;;
-        "software Update")
-            echo "in dev"
-	    ;;
-        "autoSSH/SMB")
-            ./autoSSH.sh
-	    ./autoSMB.sh
-            ;;
-	"TBD")
-	    echo "To Be Determined"
-	    ;;
-        "TBD")
-            echo "To Be Determined"
-	    ;;
-   	"TBD")
-            echo "To Be Determined"
-	    ;;
-        "Quit")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
-    # echo "breakout CASE"
-done
-echo "breakout SELECT"
