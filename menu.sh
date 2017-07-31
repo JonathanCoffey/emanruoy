0d3f5d142f46802613be1b3ac930dd4970fbd46f
JonathanCoffey <JonathanCoffey1087@gmail.com>
2017-07-30 20:59:04 -0500

    Working on tmux menu.

--- menu.sh
+++ menu.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 clear
 
-#tmux new-session -s Menu 
 
 # Declare Variables
+echo "Declaring variables..."
 HOSTNAME=$(hostname)
 ID=$(id -u)
 IPADDRESS=$(hostname -I | awk '{print $1}')
-jonSTATUS=$(tmux ls | grep "JonThan" | grep -i "attached" >/dev/null; echo $?)
-mainSTATUS=$(tmux ls | grep "Main" | grep -i "attached" >/dev/null; echo $?)
-tmuxCHECK=$(apt-cache policy tmux | grep "Installed:" | grep '(none)' >/dev/null; echo $?) # 0 value if tmux not installed. 
+jonSTATUS=$(tmux ls 2>/dev/null | grep "JonThan" | grep -i "attached" >/dev/null; echo $?)
+mainSTATUS=$(tmux ls 2>/dev/null | grep "Main" >/dev/null; echo $?) # tmux Main session already running? true or false.
+mainATTACHED=$(tmux ls 2>/dev/null | grep "Main" | grep -i "attached" >/dev/null; echo $?)
 
 
 
-# check if sudo. I left this here so that you can soure the script variables and stop the script here.;
+# check if sudo. I left this here so that you can soure the script variables and stop the script here.
+echo "Checking for sudo..."
 [ "$ID" -ne 0 ] && echo "This should be run as root." && sleep 2 && exit
 
-# test if IP address is zero value.
-[ -z "${IPADDRESS}" ] && echo "IP address is a zero value, make sure you're on network and have a valid IP." && sleep 2 && exit
 
-
-echo "Running as root. Your IPV4 is: ${IPADDRESS}"
-sleep 2
-
-# check if tmux installed.
-[ "${tmuxCHECK}" = 0 ] && echo "Installing tmux..." && apt install tmux -y >/dev/null || echo "Tmux is already installed."
+# see if already in a tmux session
+echo "Checking if  already in a tmux session..."
+if [ ! "${TMUX}" ]; then
+	echo "Not currently in a tmux session..."
+else
+	echo "Already in a tmux session..."
+	sleep 2 && exit
+fi
 sleep 3
 
+# test if IP address is zero value.
+echo "Ckecking if IP address is zero..."
+[ -z "${IPADDRESS}" ] && echo "IP address is a zero value, make sure you're on network and have a valid IP." && sleep 2 && exit
 
 # See if any tmux sessions already running, if so, attach to it instead of starting new session.
-#[ "$mainSTATUS" = 0 ] && tmux a -t "Main:menu" || tmux new-session -s "Main" $0 \; rename window "menu" \; set mouse on $
-#[ "$jonSTATUS" = 0 ] && tmux a -t "JonThan:menu" || tmux new-session -s "JonThan" $0 \; rename window "menu" \; set mouse on $
+echo "Checking if tmux already running..."
+[ "$mainSTATUS" = 0 ] && tmux a -t "Main:menu" || tmux new-session -s "Main"  \; rename-window "menu" \; set mouse on \;
+#[ "$jonSTATUS" = 0 ] && tmux a -t "JonThan:menu" || tmux new-session -s "JonThan" $0 \; rename-window "menu" \; set mouse on $
+
+#tmux new-session -s Menu
 
 # check if in tmux menu
 #if [ ! "$TMUX" ]; then
@@ -42,12 +49,5 @@ sleep 3
 #fi
 
 
-# Structure
-
-# Is tmux installed? If not, install it. 
-
-# Is "Main" already an existing tmux session?
-# # Does the user want to attach or end the already running one? 
-  #
-
-# Create new tmux session named "Main".
+echo "END OF FILE."
+exit
