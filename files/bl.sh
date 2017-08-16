16b53c6ffe2b413ef2e42056cb81700f8ef3e156
JonathanCoffey <JonathanCoffey1087@gmail.com>
2017-08-15 21:16:52 -0500

    fixed variables in bl.sh to be updated correctly in the loop, instead of only when the loop starts.

--- files/bl.sh
+++ files/bl.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-# comment
-
-bat0=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "percentage:" | cut -d ':' -f 2)
-bat1=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep "percentage:" | cut -d ':' -f 2)
-
-
+# Simple battery loop to grab percentage of charge remaining1
 
 
 echo "Starting battery loop"
 while (sleep 5); do
+  bat0=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "percentage:" | cut -d ':' -f 2)
+  bat1=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep "percentage:" | cut -d ':' -f 2)
   clear
   echo "B1:""${bat0}"
   echo "B2:""${bat1}"
+  acpi -t
+  acpi -a
+  acpi -b
 done
+
+#########################################################################
+# additional energy info, will be separated later into individual scripts
+# Thermal info
+acpi -t
+# Adapter or AC info
+acpi -a
+# battery info to be cross-referenced with upower later
+acpi -b
 
