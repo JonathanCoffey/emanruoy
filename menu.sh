14a974357531afb992b0c7aea7de107c913ec992
JonathanCoffey <JonathanCoffey1087@gmail.com>
2017-10-08 23:47:18 -0500

    Added case statement for arguments to be passed to this script. Only switch so far is "-d" for detaching users prior to attching to a tmux session.

--- menu.sh
+++ menu.sh
@@ -6,17 +6,23 @@ y=$(type tmux 2>/dev/null)
 [ ! "$y" ] && echo "Installing tmux with apt..." && sudo apt install -yqq tmux >/dev/null 2>/dev/null
 clear
 
+p1() {
 # list already running tmux sessions
 echo "CURRENTLY RUNNING SESSIONS"
 echo '-------------------------------'
 tmux ls
 echo '-------------------------------'
+}
 
+q1() {
 # Declare Variables
 read -p 'Enter a session name: ' sessionName # Asks user to name a tmux session, stores it in sessionName.
 status=$(tmux ls 2>/dev/null | grep "${sessionName}" >/dev/null; echo $?) # checks if sessionName already running. 0 if yes, 1 if not.
+}
 
-
+soloMode() {
+p1
+q1
 if [ ! "${TMUX}" ]; then # TMUX variable only exists in tmux. The test is saying: if not true, then... but if it is true, then this is false and goes to ELSE. 
   if [ "${status}" = 0 ]; then # if 0, then the chosen tmux session is already up.
     tmux detach -s "${sessionName}"
@@ -31,7 +37,7 @@ else
   if [ "${input1}" = "y" ]; then
     clear
     tmux detach -s "${sessionName}" 2>/dev/null
-    tmux new-session -d -s "${sessionName}" \; set mouse on 2>/dev/null \; && tmux switch-client -t "${sessionName}" || tmux switch-client -t "${sessionName}"
+    tmux new-session -d -s "${sessionName}" \; set mouse on 2>/dev/null \; && tmux switch-client -t "${sessionName}" || tmux switch-client -t "${sessionName}" \; set mouse on 2>/dev/null
   elif [ "${input1}" = "n" ]; then
     clear
     exit 
@@ -48,6 +54,62 @@ else
     clear
   fi
 fi
+}
+
+normalMode() {
+p1
+q1
+if [ ! "${TMUX}" ]; then
+  if [ "${status}" = 0 ]; then
+    tmux a -t "${sessionName}"
+    clear
+  else
+    tmux new-session -s "${sessionName}" \; set mouse on 2>/dev/null \;
+    tmux a -t "${sessionName}"
+  fi
+else
+  read -p 'Switch sessions? Y/n ' input1
+  if [ "${input1}" = "y" ]; then
+    if [ "${status}" = 0 ]; then
+      clear
+      tmux switch-client -t "${sessionName}" \; set mouse on \;
+    else
+      clear
+      tmux new-session -d -s "${sessionName}" \; set mouse on \;
+      tmux switch-client -t "${sessionName}"
+    fi
+  elif [ "${input1}" = "n" ]; then
+    clear
+    exit
+  elif [ "${input1}" = "" ]; then
+    clear
+    echo "Invalid option, please enter 'y' or 'n'"
+    sleep 1
+    clear
+  fi
+fi  
+}
 
 
-# Make option to NOT disconnect users when switching. 
+
+###################################### START OF SCRIPT ######################################
+
+
+case $1 in 
+  -d) # start by detaching users and running as a single user.
+    soloMode
+    exit
+    ;;
+  "") # start normally if no args are passed.
+    normalMode
+    exit
+    ;;
+  *) # all other args
+    clear
+    echo "Correct switches for $0 are"':' '-d'
+    echo "Switches are not required though, this script can be run with no args."
+    sleep 1
+    exit
+esac
+
+
