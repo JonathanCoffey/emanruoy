6905411949bfde427efa5133f4615549afb5fbe6
JonathanCoffey <JonathanCoffey1087@gmail.com>
2017-10-28 00:36:52 -0500

    adding template files. I think.

--- menu.sh
+++ menu.sh
@@ -1,4 +1,97 @@
 #!/bin/bash
+#####################################################################
+####
+#### Beginning new script code here
+####
+#####################################################################
+usage_msg() {
+clear
+echo "
+Program: menu.sh
+
+The menu.sh script is designed to manage and control muliple tmux
+sessions at a time. You can easily change contextually through 
+different menus or layouts.
+
+Usage: ${1##*/} [-?vV] [-m message]
+
+ Where:
+    -m = message - change the message to display
+    -v = Verbose mode - displays displayms function info
+    -V = Very Verbose Mode - debug output displayed
+    -? = Help - display this message
+
+Author: Jonathan Coffey (JonathanCoffey1087@gmail.com)
+
+\"AutoContent\" enabled
+\"Multi-Shell\" enabled
+"
+}
+#####################################################################
+####
+#### Description:
+####
+#### Place fell text description of shell function here.
+####
+#### Assumptions:
+####
+#### Provide list of assumptions your shell function makes,
+#### with a description of each assumption.
+####
+#### Dependencies:
+####
+#### tmux , acpi , UPower , htop , openssh-server , ssh
+#### stress, memtester, openglmark2 , xawtv , motion
+#### cvlc , vlc , xplayer , smartmontools
+####
+#### Products: 
+#### 
+#### outputs machine information commonly needed for computer diagnosis
+#### and repair. Some outputs produced by this script are S.M.A.R.T
+#### data, temperature and sensor information, webcam and camera tools
+#### vlc for playback of test audio files, hardware testing sofwtware
+#### will be used for burn-in and testing. 
+####
+#### Configured Usage:
+####
+#### Describe how your shell function should be used.
+####
+#### Details:
+####
+#### Place nothing here, the details are your shell function.
+####
+#####################################################################
+configure_displayms()
+{
+####
+#### Notice this function is a POSIX function so that it can see local
+#### and global variables from calling functions and scripts.
+####
+#### Configuration parameters can be stored in a file and
+#### this script can be dynamically reconfigured by sending
+#### the running script a HUP signal using the kill command.
+#### ex.) kill -1 <pid>
+####
+#### Configuration variables can be defined in the configuration file using
+#### the same syntax as defining a shell variable, e.g.: VARIABLE="value"
+
+  CFILE=~/.displayms.conf
+
+  (( VERBOSE == TRUE )) && echo "# Configuration File: ${CFILE}"
+
+  if [[ -f ${CFILE} ]]; then
+    (( VERBOSE == TRUE )) && cat ${CFILE}
+    . ${CFILE}
+  fi
+
+  return 0
+}
+
+#####################################################################
+####
+#### End of new script code here
+####
+#####################################################################
 clear
 
 # check if tmux installed
@@ -94,10 +187,14 @@ fi
 
 ###################################### START OF SCRIPT ######################################
 
-
+scr1() {
 case $1 in 
   -d) # start by detaching users and running as a single user.
     soloMode
+    exit
+    ;;
+  -sys) # start system menu template
+    ./files/templates/system.sh
     exit
     ;;
   "") # start normally if no args are passed.
@@ -111,5 +208,10 @@ case $1 in
     sleep 1
     exit
 esac
+} 
 
+scr1 $1
 
+# Fix issue. When passing -d switch and giving the name of the session you're attached to should
+# prompt user that perhaps they gave the wrong session name. There is no reason to detach
+# intentionally this way, there are already commands for that. 
